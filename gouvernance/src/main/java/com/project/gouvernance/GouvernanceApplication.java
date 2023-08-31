d89da71b8d08c453f144e119f735bc5e7cb1962c
Jexblue <91774746+Jexblue@users.noreply.github.com>
2023-08-31 21:25:39 +0300

    Push

---
+++ gouvernance/src/main/java/com/project/gouvernance/GouvernanceApplication.java
@@ -0,0 +1,13 @@
+package com.project.gouvernance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GouvernanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GouvernanceApplication.class, args);
+	}
+
+}
---
+++ gouvernance/src/test/java/com/project/gouvernance/GouvernanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gouvernance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GouvernanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
